1225cc422397dc3c91877c3621a0ce419a6af723
KAVIPRIYA <kavipriya.tm.2004@gmail.com>
2025-03-18 10:05:43 +0530

    Update app.py

--- AI_SocialMedia_Trend_Predictor/src/app.py
+++ AI_SocialMedia_Trend_Predictor/src/app.py
@@ -4,18 +4,16 @@ import os
 
 app = Flask(__name__)
 
-# Endpoint to get sentiment data
 @app.route('/sentiment-data', methods=['GET'])
 def get_sentiment_data():
-    df = pd.read_csv('data/processed/AI_cleaned_tweets.csv')  # Your sentiment analysis data
-    sentiment_counts = df['sentiment'].value_counts().to_dict()  # Get sentiment counts (positive, neutral, negative)
-    return jsonify(sentiment_counts)  # Send it as JSON
+    df = pd.read_csv('data/processed/AI_cleaned_tweets.csv')  
+    sentiment_counts = df['sentiment'].value_counts().to_dict()  
+    return jsonify(sentiment_counts) 
 
-# Endpoint to get the word cloud image URL
+
 @app.route('/word-cloud', methods=['GET'])
 def get_word_cloud():
-    # You already saved the word cloud image as 'word_cloud.png' in 'outputs/figures'
-    return jsonify({"image_url": "/outputs/figures/word_cloud.png"})  # Send the URL of the word cloud image
 
+    return jsonify({"image_url": "/outputs/figures/word_cloud.png"}) 
 if __name__ == '__main__':
     app.run(debug=True)
